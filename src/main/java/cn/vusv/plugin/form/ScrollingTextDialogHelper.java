4a4d13aca7f53c01934363bd35fc6455a89bdebe
Mcayear <mcayear@163.com>
2024-05-17 07:42:58 +0800

    fix: 窗口不再处理非按钮的响应

--- src/main/java/cn/vusv/plugin/form/ScrollingTextDialogHelper.java
+++ src/main/java/cn/vusv/plugin/form/ScrollingTextDialogHelper.java
@@ -1,14 +1,20 @@
 package cn.vusv.plugin.form;
 
 import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.form.element.ElementDialogButton;
+import cn.nukkit.form.response.FormResponseDialog;
 import cn.nukkit.form.window.FormWindowDialog;
 import cn.nukkit.form.window.ScrollingTextDialog;
+import cn.nukkit.network.protocol.NPCDialoguePacket;
+import cn.vusv.plugin.SuperDialogPlugin;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ScrollingTextDialogHelper {
 
@@ -22,10 +28,12 @@ public class ScrollingTextDialogHelper {
         FormWindowDialog dialog = new FormWindowDialog(title, description, entity);
 
         for (int i = 1; i <= optionsTable.length(); i++) {
-            dialog.addButton(optionsTable.get(i).tojstring());
+            String text = optionsTable.get(i).tojstring();
+            dialog.addButton(new ElementDialogButton(text, text, null, ElementDialogButton.Mode.BUTTON_MODE));
         }
 
         dialog.addHandler((player_, response) -> {
+            if (!response.getRequestType().name().equals("EXECUTE_ACTION")) return;
             int index = dialog.getButtons().stream()
                     .map(ElementDialogButton::getText)
                     .toList()
@@ -33,18 +41,34 @@ public class ScrollingTextDialogHelper {
 
             String actionName = actionsTable.get(index + 1).tojstring();
             LuaValue action = globals.get(actionName);
-            if (!action.isnil()) {
+            close(player, response);
 
-                globals.set("player", CoerceJavaToLua.coerce(player));
-                globals.set("entity", CoerceJavaToLua.coerce(entity));
-                LuaValue actionResult = action.call();
-                if (!actionName.equals("close") && !actionResult.isnil()) {
-                    createAndSendDialog(player, entity, actionResult, globals);
+            Server.getInstance().getScheduler().scheduleDelayedTask(SuperDialogPlugin.getInstance(), () -> {
+                if (!action.isnil()) {
+                    globals.set("player", CoerceJavaToLua.coerce(player));
+                    globals.set("entity", CoerceJavaToLua.coerce(entity));
+                    LuaValue actionResult = action.call();
+                    if (!actionName.equals("close") && !actionResult.isnil()) {
+
+                        createAndSendDialog(player, entity, actionResult, globals);
+                    } else {
+                        LuaValue closeAction = globals.get("close");
+                        if (!closeAction.isnil()) {
+                            LuaValue closeActionResult = closeAction.call();
+                        }
+                    }
                 }
-            }
+            }, 7);
         });
 
-        ScrollingTextDialog form = new ScrollingTextDialog(player, dialog, 1);
-        form.send(player);
+        //ScrollingTextDialog form = new ScrollingTextDialog(player, dialog, 1);
+        dialog.send(player);
+    }
+    public static void close(Player player, FormResponseDialog response) {
+        NPCDialoguePacket closeWindowPacket = new NPCDialoguePacket();
+        closeWindowPacket.setUniqueEntityId(response.getEntityRuntimeId());
+        closeWindowPacket.setAction(NPCDialoguePacket.Action.CLOSE);
+        closeWindowPacket.setSceneName(response.getSceneName());
+        player.dataPacket(closeWindowPacket);
     }
 }
